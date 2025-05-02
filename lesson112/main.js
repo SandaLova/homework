1a43c80ace13c5c6abd0e1caabe237fe00e38679
SandaLova <sandalova_e.n@icloud.com>
2025-05-02 10:11:07 +0500

    add lesson112

---
+++ lesson112/main.js
@@ -0,0 +1,29 @@
+// -------первый вариант--------//
+// const openButton = document.getElementById('open-modal');
+// const closeButton = document.getElementById('close-modal');
+// const modalWindow = document.getElementById('modal-window');
+
+// // Обработчик события для кнопки открытия модального окна
+// openButton.addEventListener('click', () => {
+//     // Открываем модальное окно
+//     modalWindow.showModal();
+// });
+
+// // Обработчик события для закрытия модального окна
+// closeButton.addEventListener('click', () => {
+//     // Закрываем модальное окно
+//     modalWindow.close();
+// });
+
+// -------второй вариант--------- //
+const openButtons = document.querySelectorAll('.open-modal')
+const closeButton = document.querySelector('.modal__close-btn')
+const modalWindow = document.querySelector('.modal')
+
+openButtons.forEach(button => button.addEventListener('click', () => {
+    modalWindow.style.display = 'block'
+}))
+
+closeButton.addEventListener('click', () => {
+    modalWindow.style.display = 'none'
+})
